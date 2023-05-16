4fd7d4d18efef6fadbbef16fa23ce1fbf4194629
Philip <philipschubert00@gmail.com>
2023-05-16 14:41:38 +0200

    Code kommentiert

--- test.py
+++ test.py
@@ -5,14 +5,14 @@ from main import unzipp
 #Setup für Download und Speicherung
 url = 'https://upload.uni-jena.de/data/641c17ff33dd02.60763151/GEO419A_Testdatensatz.zip'
 #path niklas
-save_path = 'D:/Studium/GEO_419A Python Kurs/Abschlussaufgabe/Ouput/test'
+#save_path = 'D:/Studium/GEO_419A Python Kurs/Abschlussaufgabe/Ouput/test'
 #path philip
-#save_path = 'C:/Users/herzu/Documents/GEO419'
+save_path = 'C:/Users/herzu/Documents/GEO419'
 
 filename = url.rsplit('/', 1)[1]
 filename_no_type = filename.split('.', 1)[0]
 
 #Aufruf der Funktionen
 
-#download_zip(url, save_path)
+download_zip(url, save_path)
 unzipp(save_path, filename)
