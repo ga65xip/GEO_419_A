55fb474c3309bce1b3b091650b417e7c853111ec
Philip <philipschubert00@gmail.com>
2023-05-17 15:51:45 +0200

    added and tested function

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 from main import download_zip
 from main import unzipp
+from main import plotting
 
 
 #Setup für Download und Speicherung
 url = 'https://upload.uni-jena.de/data/641c17ff33dd02.60763151/GEO419A_Testdatensatz.zip'
 #path niklas
-save_path = 'D:/Studium/GEO_419A Python Kurs/Abschlussaufgabe/Ouput/test'
+#save_path = 'D:/Studium/GEO_419A Python Kurs/Abschlussaufgabe/Ouput/test'
 #path philip
-#save_path = 'C:/Users/herzu/Documents/GEO419'
+save_path = 'C:/Users/herzu/Documents/GEO419'
 
 filename = url.rsplit('/', 1)[1]
 filename_no_type = filename.split('.', 1)[0]
@@ -15,4 +16,5 @@ filename_no_type = filename.split('.', 1)[0]
 #Aufruf der Funktionen
 
 #download_zip(url, save_path)
-unzipp(save_path, filename)
+#unzipp(save_path, filename)
+plotting(save_path)
