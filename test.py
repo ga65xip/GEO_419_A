e7a9b2bdd396cf0851be416661c49b1517046902
Philip <philipschubert00@gmail.com>
2023-06-13 13:37:25 +0200

    changed to program

--- test.py
+++
@@ -1,9 +0,0 @@
-from main import start_program
-from pathlib import Path
-# Setup für Download und Speicherung
-# Philip
-# save_path = 'C:/Users/herzu/Documents/GEO419'
-# Niklas
-path = r'D:\Studium\GEO_419A Python Kurs\Abschlussaufgabe\Ouput\test'
-save_path = Path(r'{}'.format(path))
-start_program(save_path)
