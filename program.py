3a8be6cd3c6932e828904a2c0f92a0cb90fbd35a
Philip <philipschubert00@gmail.com>
2023-06-19 16:46:47 +0200

    Changed Comment

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 from main import start_program
 from pathlib import Path
 
-path = r'D:\Studium\GEO_419A Python Kurs\Abschlussaufgabe\Ouput\test'
+path = r'C:\Users\herzu\Documents\GEO419'
 save_path = Path(r'{}'.format(path))
 
 start_program(save_path)
